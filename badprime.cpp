334ac1982151d3d75bbc2f3ab501b627157b238b
Luciano Silva <luciano.silva.sp@gmail.com>
2023-03-24 13:20:21 -0300

    Add files via upload

---
+++ badprime.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+bool isPrime(int x)
+{
+  int limit = std::sqrt(x);
+  for (int i = 2; i <= limit; ++i)
+  {
+    if (x % i == 0)
+    {
+      return false;
+    }
+  }
+  return true;
+}
+
+int main()
+{
+  int primeCount = 0;
+  for (int i = 0; i < 1000000; ++i)
+  {
+    if (isPrime(i))
+    {
+      ++primeCount;
+    }
+  }
+}
